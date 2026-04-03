2720a8b2e8033acbefda8da5c0a71cd32238c78e
Kartik Trivedi <kartik0trivedi@gmail.com>
2026-04-03 00:00:38 -0400

    feat: update site URL and implement RSS feed content rendering with markdown-it and sanitize-html

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,10 @@
-import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+
+const parser = new MarkdownIt();
 
 export async function GET(context) {
 	const posts = await getCollection('writing');
@@ -11,6 +15,10 @@ export async function GET(context) {
 		items: posts.map((post) => ({
 			...post.data,
 			link: `/writing/${post.id}/`,
+			// Note: The new Content Layer entries have a 'body' property 
+			// if and only if you're using Markdown/MDX files.
+			content: sanitizeHtml(parser.render(post.body || '')),
 		})),
 	});
 }
+
